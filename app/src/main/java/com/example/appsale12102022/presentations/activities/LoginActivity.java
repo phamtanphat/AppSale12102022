4fcda2d7160937abc5fb9ebad628da3c0a4c55b0
Phat <phatdroid94@gmail.com>
2023-02-01 18:12:16 +0700

    view binding

--- app/src/main/java/com/example/appsale12102022/presentations/activities/LoginActivity.java
+++ app/src/main/java/com/example/appsale12102022/presentations/activities/LoginActivity.java
@@ -3,14 +3,21 @@ package com.example.appsale12102022.presentations.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.appsale12102022.R;
+import com.example.appsale12102022.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
     }
 }
